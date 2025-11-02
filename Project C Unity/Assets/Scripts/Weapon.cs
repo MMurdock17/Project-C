0cea0aeef8b3825e5e5317818a3a6507565fbda2
MMurdock17 <mattiemurdock1@gmail.com>
2025-11-01 22:25:04 -0400

    Setting particles for hit

--- Project C Unity/Assets/Scripts/Weapon.cs
+++ Project C Unity/Assets/Scripts/Weapon.cs
@@ -6,6 +6,7 @@ public class Weapon : MonoBehaviour
     public int damage;
     public Camera camera;
     public float fireRate;
+    public GameObject hitVFX;
 
     private float nextFire;
 
@@ -35,6 +36,8 @@ public class Weapon : MonoBehaviour
 
         if (Physics.Raycast(ray.origin, ray.direction, out hit, 100f))
         {
+            PhotonNetwork.Instantiate(hitVFX.name, hit.point, Quaternion.identity);
+
             if (hit.transform.gameObject.GetComponent<Health>())
             {
                 hit.transform.gameObject.GetComponent<PhotonView>().RPC("TakeDamage", RpcTarget.All, damage);
