0c8bf2eb1df4bc462b8ce3f4fc9d65e92387db66
MMurdock17 <mattiemurdock1@gmail.com>
2025-11-01 21:57:17 -0400

    Damaging other players

---
+++ Project C Unity/Assets/Scripts/Health.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using Photon.Pun;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+
+    [PunRPC]
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
